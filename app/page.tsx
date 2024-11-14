45cbb6abae9158ee4de29f027e90999f52b7f0c6
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2024-11-14 20:16:23 +0530

    feat: add live cursor

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="h-[100vh] w-full flex justify-center items-center text-center">
-      <h1 className="text-5xl text-white">Liveblocks figma clone</h1>
+    <div>
       <Live />
     </div>
   );
--- components/Live.tsx
+++ components/Live.tsx
@@ -13,16 +13,34 @@ const Live = () => {
     const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
     // broadcast the cursor position to other users
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
+  const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
     updateMyPresence({
-      cursor: {
-        x,
-        y,
-      },
+      cursor: null,
+      message: null,
+    });
+  }, []);
+
+  const handlePointerDown = useCallback((event: React.PointerEvent) => {
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({
+      cursor: { x, y },
     });
   }, []);
 
   return (
-    <div>
+    <div
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+      onPointerDown={handlePointerDown}
+      className="h-[100vh] w-full flex justify-center items-center text-center border-2"
+    >
+      <h1 className="text-5xl text-white">Liveblocks figma clone</h1>
       <LiveCursors others={others} />
     </div>
   );
